22132f092f754e2f8d2c0afd209bf35189ebff9a
Eleazar <eleazar@Toblerone.local>
2023-09-26 11:39:36 -0700

    starting assignment

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import {SERVER_URL} from '../constants';
 
+// Front end code to create a new assignment by entering
+// course_id, assignment name and due date. 
+// Check the format of the due date for the form yyyy-mm-dd.
 
 function AddAssignment(props) { 
-
+  const [assignment, createAssignment] = useState([]);
 
   return (
       <div>
